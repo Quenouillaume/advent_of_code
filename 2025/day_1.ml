6bb51887e3b6b7b023c9cc640bbef3108eb81968
G. Rousseau <guillaume.rousseau@ac-toulouse.fr>
2025-12-01 06:48:53 +0100

    j1 2025

--- 2025/day_1.ml
+++ 2025/day_1.ml
@@ -1,10 +1,23 @@
-
-open Advent_of_code;;
+open Advent_of_code
 let test = false
 let data = get_input 2025 1
 
+
+let rec sum_rot l p =
+    match l with 
+    | [] -> 0
+    | (b, x) :: q -> sum_rot q ((((p + (if b then x else -x)) mod 100) + 100) mod 100) + 
+        if b && p + x >= 100 then (p+x) / 100 
+        else if not b && p - x <= 0 then (if p > 0 then 1 else 0) + (-p+x)/100
+        else 0
+
 let day_1 (data: string list) =
-    0
+    let l = data 
+    |> List.map (fun s -> ((s.[0]  = 'R'), String.sub s 1 (String.length s - 1)))
+    |> List.map (fun (b, s) -> (b, int_of_string s))
+    in sum_rot l 50
+
+(* let _ = List.iter (fun s -> print_string s; print_string "\n") data *)
 
 let main () =
     let sol = if test then -1 else day_1 data in
