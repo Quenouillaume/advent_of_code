e1f3b64f68e7fad3ce4ec05e7eba6e3482c97e07
G. Rousseau <guillaume.rousseau@ac-toulouse.fr>
2025-12-03 09:08:57 +0100

    j3 fast

--- 2025/day_3.ml
+++ 2025/day_3.ml
@@ -42,16 +42,52 @@ let rec n_jolt_max (t: int array) (k: int) =
         end
     in calc_jolt 0 k
 
+(* Part 2 faster: O(n * B) where B is the base used (here B=10)
+   precompute lookup table N[d][i] = first index >= i where digit d appears
+   with d < B and i < n
+ *)
+ 
+let fast_jolt (t: int array) (k: int): int =
+    let n = Array.length t in
+    let next = Array.make_matrix 10 (n+1) n in
+    (* next[d][i] = first index > i where digit d appears *)
+    for i = n-1 downto 0 do
+        for d = 0 to 9 do
+            next.(d).(i) <-
+                if t.(i) <> d then next.(d).(i+1)
+                else i;
+        done
+    done;
+    let i = ref 0 in
+    let p10 = ref (pow 10 (k-1)) in
+    let k = ref k in 
+    let res = ref 0 in
+    while !k > 0 do
+        assert(!i < n);
+        let d = ref 9 in 
+        while (!d >= 0 && n-next.(!d).(!i) < !k) do
+            decr d
+        done;
+        res := !res + !p10 * !d;
+        p10 := !p10 / 10;
+        i := next.(!d).(!i) + 1;
+        decr k;
+    done;
+    !res
+
+    
+
+
 let day_3 (data: string list) =
     data |> List.map String.to_seq |> List.map List.of_seq
     |> List.map (List.map int_of_char) 
     |> List.map (List.map (Fun.flip (-) (int_of_char '0')))
     |> List.map (Array.of_list)
-    |> List.map (Fun.flip n_jolt_max 12)
+    |> List.map (Fun.flip fast_jolt 12)
     |> List.fold_left (+) 0
 
 let main () =
-    let sol = if test then -1 else day_3 tdata in
+    let sol = if test then -1 else day_3 data in
 
     if sol > 0 then begin
         print_int sol; print_newline ();
---
+++ TIME.sh
@@ -0,0 +1,10 @@
+# Compute total time for solving AOC 
+
+echo > out_time.txt
+for (( i = 1; i <= 3; i++ )); do
+	ocamlc unix.cma advent_of_code.ml 2025/day_$i.ml -w -A
+	(time ./a.out) 2>> out_time.txt
+done
+
+cat out_time.txt | grep real | awk '{print $2}'
+rm a.out out_time.txt
