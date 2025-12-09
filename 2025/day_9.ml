06305d13c7386102070033171484a2439e1b767c
G. Rousseau <guillaume.rousseau@ac-toulouse.fr>
2025-12-09 12:53:19 +0100

    j9 quadtree

--- 2025/day_9.ml
+++ 2025/day_9.ml
@@ -93,6 +93,99 @@ let rec find_rect (seqs: (pt*pt) list) (pairs: (int*pt*pt) list) : int =
         a 
 
 
+(* Part 2 faster: Quadtree ??? O(n^2 log n) on average *)
+let leaf_size = 10
+type qt =
+    | Leaf of pt * pt * int * pt list (* bounding box, then nb of points in leaf, then points *)
+    | Node of pt * pt * qt array array (* bounding box, then four children *)
+
+(* Return true if point 3 is inside rectangle made of points 1 and 2.*)
+let soft_intersect (x1, y1) (x2, y2) (x3, y3) : bool =
+    min x1 x2 <= x3 && x3 <= max x1 x2 && min y1 y2 <= y3 && y3 <= max y1 y2
+
+
+(* return 0-1 index of subtree where (x, y) belongs
+   in QT of bounding box (x1, y1, x2, y2) *)
+let child_pos (x1, y1) (x2, y2) (x, y): int* int =
+    let xm = (x2+x1) / 2 in
+    let ym = (y2+y1) / 2 in
+    let i = if x < xm then 0 else 1 in
+    let j = if y < ym then 0 else 1 in
+    i, j
+
+let subdiv (q: qt) = match q with
+    | Leaf ((x1, y1), (x2, y2), _, _) ->
+        let xm = (x2+x1) / 2 in
+        let ym = (y2+y1) / 2 in
+        let child i j =
+            let xa = if i = 0 then x1 else xm in
+            let xb = if i = 0 then xm-1 else x2 in
+            let ya = if j = 0 then y1 else ym in
+            let yb = if j = 0 then ym-1 else y2 in
+            Leaf ((xa, ya), (xb, yb), 0, [])
+        in 
+        let children =
+            Array.init 2 (fun i -> Array.init 2 (fun j -> child i j))
+        in
+        Node ((x1, y1), (x2, y2), children)
+    | _ -> failwith "Cannot subdiv node"
+
+
+let rec insert (q: qt) ((x, y) as p: pt) : qt =
+    match q with
+    | Leaf ((x1, y1), (x2, y2), n, l) when n < leaf_size 
+        -> Leaf((x1, y1),(x2, y2), n+1, (x, y) :: l)    
+    | Leaf ((x1, y1), (x2, y2), n, l) -> begin
+        let i, j = child_pos (x1, y1) (x2, y2) p in
+        match subdiv q with 
+        | Node(_, _, children) as q' -> begin
+            children.(i).(j) <- insert children.(i).(j) (x, y);
+            List.iter 
+                (fun ((x', y') as p') ->
+                    let i', j' = child_pos (x1, y1) (x2, y2) p' in
+                    children.(i').(j') <- insert children.(i').(j') (x', y');
+                )
+                l;
+            q'
+        end
+        | _ -> failwith  "Should not happen: leaf after subdiv"
+    end
+    | Node((x1, y1), (x2, y2), children) ->
+        let i, j = child_pos (x1, y1) (x2, y2) p in
+        children.(i).(j) <- insert children.(i).(j) (x, y);
+        q
+
+
+(* return true if any point of q is strictly inside rectangle p1 p2 *)
+let rec insides (q: qt) (p1: pt) (p2: pt) : bool =
+    match q with
+    | Leaf(((xa, ya) as a), ((xb, yb) as b), n, l) ->
+        if not (soft_intersect p1 p2 a || soft_intersect p1 p2 b || soft_intersect a b p1 || soft_intersect a b p2)
+        then false
+        else List.exists (intersect p1 p2) l
+    | Node(((xa, ya) as a), ((xb, yb) as b), children) ->
+        if not (soft_intersect p1 p2 a || soft_intersect p1 p2 b || soft_intersect a b p1 || soft_intersect a b p2)
+        then false
+        else Array.exists (Array.exists (fun q' -> insides q' p1 p2)) children
+
+let rec find_rect (seqs: (pt*pt) list) (pairs: (int*pt*pt) list) (qtree: qt) : int =
+    match pairs with 
+    | (a, p1, p2) :: q -> 
+    let x1 = min (fst p1) (fst p2) in
+    let x2 = max (fst p1) (fst p2) in
+    let y1 = min (snd p1) (snd p2) in
+    let y2 = max (snd p1) (snd p2) in
+    if insides qtree (x1, y1) (x2, y2) then find_rect seqs q qtree else
+    if
+        List.exists (fun (p3, p4) ->
+            is_conflict (get_position p1 p2 p3) (get_position p1 p2 p4)
+        )
+        seqs 
+    then
+        find_rect seqs q qtree
+    else
+        a 
+
 
 let day_9 (data: string list) =
     let pts = 
@@ -102,8 +195,17 @@ let day_9 (data: string list) =
         prod pts pts |> List.rev_map (fun (p1, p2) -> (area p1 p2, p1, p2))
         |> List.sort compare 
         |> List.rev
-    in let seqs = list_seqs pts
-    in find_rect seqs pairs
+    in 
+    let xs = List.map fst pts in
+    let ys = List.map snd pts in
+    let minx = List.fold_left min (List.hd xs) xs in
+    let maxx = List.fold_left max (List.hd xs) xs in
+    let miny = List.fold_left min (List.hd ys) ys in
+    let maxy = List.fold_left max (List.hd ys) ys in
+
+    let qtree = List.fold_left insert (Leaf((minx, miny), (maxx, maxy), 0, [])) pts in
+    let seqs = list_seqs pts in 
+    find_rect seqs pairs qtree
     (* in pts, seqs, pairs *)
  
 
